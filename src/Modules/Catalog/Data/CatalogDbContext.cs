fa4e0c355a2c32f376161f14aec50278a663d28b
Rodolpho Trinca <rodolphotrinca@hotmail.com>
2025-04-23 21:30:33 -0300

    Configure docker compose with postgres and starting setup dbContext

---
+++ src/Modules/Catalog/Data/CatalogDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.Data;
+
+public class CatalogDbContext : DbContext
+{
+    public DbSet<Product> Products => Set<Product>();
+
+    public CatalogDbContext(DbContextOptions<CatalogDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasDefaultSchema("catalog");
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+}
--- src/Modules/Catalog/GlobalUsings.cs
+++ src/Modules/Catalog/GlobalUsings.cs
@@ -1,2 +1,6 @@
 ﻿global using Shared.DDD;
 global using Catalog.Products.Models;
+global using Catalog.Products.Events;
+global using Microsoft.EntityFrameworkCore;
+global using System.Reflection;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
