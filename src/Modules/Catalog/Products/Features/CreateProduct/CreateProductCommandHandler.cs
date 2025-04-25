6832c34a9b0255b88d180d1f62b2ae8209525e64
Rodolpho Trinca <rodolphotrinca@hotmail.com>
2025-04-24 23:15:40 -0300

    Defining the contracts of CQRS mediatR

---
+++ src/Modules/Catalog/Products/Features/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.Products.Features.CreateProduct;
+
+public record CreateProductCommand(string name, List<string> category, string description, string imageFile, decimal price) 
+    : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
